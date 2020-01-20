a8e0d1344e35b3be78f6ac789c4f3955dc9e9eec
Tammy Lam <lamtammy1515@gmail.com>
2020-01-20 19:19:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,11 +2,11 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 
-protein= gets.chomp
 
-def meal_choice(veg1="broccoli", veg2="macaroni", protein=gets.chomp)
+
+def meal_choice(veg1="broccoli", veg2="macaroni", protein='meat')
   puts "What a nutritious meal!"
  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
 
-meal_choice(veg1='broccoli', veg2='macaroni', protein='tofu')
+meal_choice("broccoli", "macaroni", "tofu")
